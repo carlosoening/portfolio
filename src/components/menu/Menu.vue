e36720b03d400f168d492f8108cf6f76106207a7
Carlos Oening <54125623+carlosoening@users.noreply.github.com>
2024-03-08 17:45:43 +0000

    Inicio criação do menu

---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,23 @@
+<script setup>
+import MenuItem from './MenuItem.vue'
+</script>
+
+<template>
+    <div class="menu">
+        <MenuItem name="Sobre"/>
+        <MenuItem name="Projetos"/>
+        <MenuItem name="Contato"/>
+    </div> 
+</template>
+
+<style scoped>
+    .menu {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        align-items: center;
+        height: 50px;
+        gap: 20px;
+        margin: 0 100px 0 100px
+    }
+</style>
---
+++ src/components/menu/MenuItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    name: String
+})
+</script>
+
+<template>
+    <div class="menu-item">
+        <a href="#">{{ name }}</a>
+    </div> 
+</template>
+
+<style scoped>
+    .menu-item {
+        display: flex;
+    }
+</style>
